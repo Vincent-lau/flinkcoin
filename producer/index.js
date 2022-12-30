fd09ffe4f01d9063144a40803a4b100c713024a4
Shuntian Liu <shuntian.liu@outlook.com>
2022-12-30 17:23:44 +0000

    feat: initial working version

--- producer/index.js
+++ producer/index.js
@@ -1,54 +1,22 @@
+const ip = require('ip')
+
+const { Kafka, CompressionTypes, logLevel } = require('kafkajs')
 const WebSocket = require('ws');
-const { Kafka } = require('kafkajs');
+
+const host = process.env.HOST_IP || ip.address()
 
 const kafka = new Kafka({
-  clientId: 'producer',
-  brokers: ['kafka:9092']
-  // brokers: [process.env.KAFKA_BROKER]
-});
+  logLevel: logLevel.DEBUG,
+  brokers: [`${host}:9092`],
+  clientId: 'example-producer',
+})
 
-const producer = kafka.producer();
+const topic = 'topic-test'
+const producer = kafka.producer()
 
 
 console.log('producer is running');
-const consumer = kafka.consumer({ groupId: 'test-group' })
 
-
-// const send = async () => {
-//   // Producing
-//   await producer.connect()
-
-//   setInterval(() => {
-//     // Producing
-//     producer.send({
-//       topic: 'test-topic',
-//       messages: [
-//         { value: 'Hello KafkaJS user!' },
-//       ],
-//     })
-//   }, 3000);
-
-// }
-
-// send().catch(console.error)
-
-const run = async () => {
-  // Consuming
-  await consumer.connect()
-  await consumer.subscribe({ topic: 'coinbase', fromBeginning: true })
-
-  await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      console.log({
-        partition,
-        offset: message.offset,
-        value: message.value.toString(),
-      })
-    },
-  })
-}
-
-run().catch(console.error)
 
 async function setup() {
   try {
@@ -81,9 +49,9 @@ async function setup() {
     }));
 
     ws.on('message', async msg => {
+      console.log(msg);
       await producer.send({
-        // topic: process.env.KAFKA_TOPIC,
-        topic: 'coinbase',
+        topic: topic,
         messages: [
           {
             value: msg
@@ -98,4 +66,98 @@ async function setup() {
 }
 
 setup();
+
+
+// const getRandomNumber = () => Math.round(Math.random(10) * 1000)
+// const createMessage = num => ({
+//   key: `key-${num}`,
+//   value: `value-${num}-${new Date().toISOString()}`,
+// })
+
+// const sendMessage = () => {
+//   return producer
+//     .send({
+//       topic,
+//       compression: CompressionTypes.GZIP,
+//       messages: Array(getRandomNumber())
+//         .fill()
+//         .map(_ => createMessage(getRandomNumber())),
+//     })
+//     .then(console.log)
+//     .catch(e => console.error(`[example/producer] ${e.message}`, e))
+// }
+
+// const run = async () => {
+//   await producer.connect()
+//   setInterval(sendMessage, 3000)
+// }
+
+// run().catch(e => console.error(`[example/producer] ${e.message}`, e))
+
+// const errorTypes = ['unhandledRejection', 'uncaughtException']
+// const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2']
+
+// errorTypes.forEach(type => {
+//   process.on(type, async () => {
+//     try {
+//       console.log(`process.on ${type}`)
+//       await producer.disconnect()
+//       process.exit(0)
+//     } catch (_) {
+//       process.exit(1)
+//     }
+//   })
+// })
+
+// signalTraps.forEach(type => {
+//   process.once(type, async () => {
+//     try {
+//       await producer.disconnect()
+//     } finally {
+//       process.kill(process.pid, type)
+//     }
+//   })
+// })
+
+
+// // const consumer = kafka.consumer({ groupId: 'test-group' })
+
+
+// // const send = async () => {
+// //   // Producing
+// //   await producer.connect()
+
+// //   setInterval(() => {
+// //     // Producing
+// //     producer.send({
+// //       topic: 'test-topic',
+// //       messages: [
+// //         { value: 'Hello KafkaJS user!' },
+// //       ],
+// //     })
+// //   }, 3000);
+
+// // }
+
+// // send().catch(console.error)
+
+
+
+// const run = async () => {
+//   // Consuming
+//   await consumer.connect()
+//   await consumer.subscribe({ topic: 'coinbase', fromBeginning: true })
+
+//   await consumer.run({
+//     eachMessage: async ({ topic, partition, message }) => {
+//       console.log({
+//         partition,
+//         offset: message.offset,
+//         value: message.value.toString(),
+//       })
+//     },
+//   })
+// }
+
+// // run().catch(console.error)
 
